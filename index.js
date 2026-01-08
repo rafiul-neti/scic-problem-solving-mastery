c09fa725de02a918126979e2934111fc639a85ca
Rafiul Islam <irafiul210@gmail.com>
2026-01-08 08:42:38 +0600

    solved the problem number 02

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ const reverseString = (string) => {
 
   return reverseArray.join("");
 };
+
+/*
+Problem 2: Count Vowels in a String
+Write a function that counts how many vowels (a, e, i, o, u) are in a given string.
+*/
+const countVowels = (string) => {
+  const vowels = ["a", "i", "o", "u", "e"];
+  let count = 0;
+  for (const vowel of string.split("")) {
+    if (vowels.includes(vowel)) count++;
+  }
+
+  return count;
+};
